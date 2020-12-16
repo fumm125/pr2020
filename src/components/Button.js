5b3677dd6bed920467ee0b4ee57bd8722b026240
Han <gksgytmd19@naver.com>
2020-12-16 17:04:21 +0900

    study button scss

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
+
+function Button({ children, size }) {
+    return <button className=
+    {classNames('Button', size)}>{children}</button>;
+}
+
+Button.defaultProps ={
+    size: 'medium',
+};
+
+
+ 
+
+export default Button;
