1d004ec902bae6cab089e700ecb5b96169bfee63
Han <gksgytmd19@naver.com>
2021-01-07 21:28:03 +0900

    프론트엔드탭 세분화

--- src/Front.js
+++ src/Front.js
@@ -6,6 +6,7 @@ import './components/Topnavi.scss';
 import './components/SidebarMb.scss';
 import './Front.scss';
 import fsec1img from './img/react.png';
+import fsec2img from './img/web.png';
 
 
 const Front =  () => {
@@ -16,27 +17,63 @@ const Front =  () => {
             <Cover></Cover>
             <SidebarMb></SidebarMb>
             <div className="Frontct">
-                <h1>
+                <h1 className="Toptext">
                     <span>프론트엔드 부분 영역</span> 
                 </h1>
-                <ol>
-                    <li className="fsec1">
-                    <div className="imgsec">
-                        <img src={fsec1img} alt='fsec1img'/>
-                    </div>
-                    <div className="textsec">
-                        <h1>REACT</h1> 
-                    </div>
-                </li>
-                <li className="fsec2">
+                <ol className="Frontinfo">
+                    <li>
+                        <div className="imgsec">
+                            <img src={fsec1img} alt='fsec1img'/>
+                        </div>
+                        <div className="textsec">
+                            <h1>React</h1>
+                            <p>모든 페이지는 리액트를 이용하여 제작하였습니다.
+                            <br></br>
+                            <br></br>
+                            이미 수많은 기업과 개발자들이 채용한 라이브러리인 React를 직접 부딪혀 다뤄보며 익혔습니다.
+                            <br></br>
+                            <br></br>
+                            리액트의 핵심이라 할 수 있는 컴포넌트를 이해하고 있으며 JSX에 대한 기초적인 이해와 사용이 가능합니다.
+                            </p>
 
-                </li>
-                <li className="fsec3">
+                        </div>
+                    </li>
+                    <li>
+                        <div className="imgsec">
+                            <img src={fsec2img} alt='fsec2img'/>
+                        </div>
+                        <div className="textsec">
+                            <h1>HTML5&nbsp;&nbsp;&nbsp;&nbsp;
+                                CSS&nbsp;&nbsp;&nbsp;&nbsp;
+                                JavaScript</h1>
+                            <p>프론트엔드의 기본이라 할 수 있는 HTML과CSS를 다룰 수 있습니다.
+                            <br></br>
+                            <br></br>
+                            웹페이지의 동적인 부분을 담당하는 JavaScript를 활용하여 HTML과CSS 만으로는 다소 밋밋한 페이지를
+                            <br></br>
+                            <br></br>
+                            시각적,기능적으로 더욱 뛰어난 페이지로 구성이 가능합니다.
+                            
 
-                </li>
-                <li className="fsec4">
-
-                </li>
+                            </p> 
+                        </div>
+                    </li>
+                    <li>
+                        <div className="imgsec">
+                            <img src={fsec2img} alt='fsec2img'/>
+                            </div>
+                            <div className="textsec">
+                            <h1>REACT</h1> 
+                            </div>
+                        </li>
+                    <li>
+                        <div className="imgsec">
+                            <img src={fsec1img} alt='fsec1img'/>
+                            </div>
+                            <div className="textsec">
+                            <h1>REACT</h1> 
+                        </div>
+                    </li>
                 </ol>
             </div>
         </div>
